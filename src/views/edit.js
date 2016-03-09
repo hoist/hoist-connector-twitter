e784dc9d77b8b56eef27d1ce5a44e4033160142f
Jamie Wilson <jamie@hoist.io>
2016-03-10 10:06:19 +1300

    new view

--- src/views/edit.js
+++ src/views/edit.js
@@ -1,3 +1,7 @@
+/* globals UI */
+
+var C = UI.Connectors;
+
 class EditForm extends React.Component {
   constructor (props) {
     super(props);
@@ -12,11 +16,22 @@ class EditForm extends React.Component {
   }
   render () {
     return (
-      <div>
-        <div className="button" onClick={() => {
-          this.connect();
-        }}>connect</div>
-      </div>
+      <C.View>
+        <C.View.Sidebar>
+          <UI.FormElements.Button text="Save" onClick={this.connect} />
+        </C.View.Sidebar>
+        <C.View.Content>
+          <h1>Adding a MYOB AccountRight Connector</h1>
+          <ul>
+            <li>Register for MYOB developer access at http://developer.myob.com/contact/register-for-myobapi-access/</li>
+            <li>Log into your my.myob.com account and choose the correct country.</li>
+            <li>Select 'Developer' from the top menu on the page.</li>
+            <li>Click on 'Register App'.</li>
+            <li>In Redirect URL use https://bouncer.hoist.io/bounce, fill out the rest of the form and click Register App.</li>
+            <li>Copy the Key into here</li>
+          </ul>
+        </C.View.Content>
+      </C.View>
     );
   }
 }
